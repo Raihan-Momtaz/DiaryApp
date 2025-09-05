9fdb592ae6a7534b8b455614ba9713c1393f6473
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-09-05 22:36:42 +1000

    Dairy Data Seeded

---
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+namespace DiaryApp.Controllers
+{
+    public class DiaryEntriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -10,7 +10,38 @@ namespace DiaryApp.Data
         {
 
         }
-        
+
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry 
+                {
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Julia!",
+                    Created = new DateTime(2025, 9, 5, 10, 0, 0)
+                },
+                new DiaryEntry 
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "Went shopping with Lisa!",
+                    Created = new DateTime(2025, 9, 5, 12, 0, 0)
+                },
+                new DiaryEntry 
+                {
+                    Id = 3,
+                    Title = "Went Diving",
+                    Content = "Went diving with Sophie!",
+                    Created = new DateTime(2025, 9, 5, 14, 0, 0)
+                }
+            );
+        }
+
     }
 }

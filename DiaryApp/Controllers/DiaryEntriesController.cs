f0b8f82a27c9d74f674370f1131bdd317b521032
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2025-09-06 01:18:00 +1000

    Form for a new Diary Entry created

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -2,7 +2,7 @@ using DiaryApp.Data;
 using DiaryApp.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace DiaryApp.Controllers
-{   
+{
     public class DiaryEntriesController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -15,6 +15,11 @@ namespace DiaryApp.Controllers
         {
             List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
             return View(objDiaryEntryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 
